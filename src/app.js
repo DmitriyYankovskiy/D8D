be152fba5e908c855b5da142f1be78264ab4b0f4
DmitriyYankovskiy <dmityank@gmail.com>
2023-07-19 22:37:41 +0300

    улучшил синтаксис

--- src/app.js
+++ src/app.js
@@ -3,7 +3,6 @@ require("../config.js");
 const express = require("express");
 const ws = require("ws");
 const http = require("http");
-const postgresql = require("pg");
 const fs = require('fs');
 const psqlm = require('./modules/psqlmanager');
 const os = require("os");
@@ -12,7 +11,7 @@ const host = "localhost";
 const httpPort = 8788;
 const wsPort = 8787;
 const d8dconfig = JSON.parse(fs.readFileSync(os.homedir()+'/.d8dconfig', 'utf8'));
-
+4
 const app = express();
 const server = http.createServer(app);
 const wss = new ws.Server({port: wsPort});
@@ -36,11 +35,11 @@ app.use("/www", express.static(global.__basedirname + "/www"));
 app.use("/home", require("../controlers/home.js"));
 
 
-/*psqlm.createTable(client,"testable",{name:"varchar", id:"int", color:"varchar"});
+psqlm.createTable(client,"testable",{name:"varchar", id:"int", color:"varchar"});
 psqlm.insertInto(client,"testable",{id:5,color:"blue"});
 console.log(psqlm.getFrom(client,["*"],"testable"));
 psqlm.deleteStrokes(client,"testable","*");
-psqlm.endClient(client);*/
+psqlm.endClient(client);
 
 server.listen(httpPort, host, ()=>{
     console.log("server was started");
--- src/modules/psqlmanager.js
+++ src/modules/psqlmanager.js
@@ -1,106 +1,106 @@
-const {Client} = require('pg');
+module.exports.getClient = (user, host, database, password, port) => {
+    const client = new require('pg')({ 
+        user: user,
+        host: host,
+        database: database,
+        password: password,
+        port: port
+    });
+    client.connect();
+    return client;
+};
 
-let exported = {
-    getClient(user, host, database, password, port){
-        const client = new Client({ 
-            user: user,
-            host: host,
-            database: database,
-            password: password,
-            port: port
-        });
-        client.connect();
-        return client;
-    },
-    getFrom(client, columns, tablename){
-        let query = `SELECT `;
-        let answer;
-        for(let i in columns){
-            query+=`${columns[i]}, `;
-        }
-        query=query.slice(0,-2);
-        query+=` FROM ${tablename};`;
-        //console.log(query);
-        client.query(query, (err, res,ans) => { 
-            if (err) {
-                console.error(err); 
-                return; 
-            } 
-            console.log(`Columns from ${tablename} is successfully got`);
-            answer=res;
-        }); 
-        return answer;
-    },
-    createTable(client, tablename, columns){
-        let query = `CREATE TABLE ${tablename} ( `;
-        for(let i in columns){
-            query+=`${i} ${columns[i]}, `;
-        }
-        query=query.slice(0,-2);
-        query+=" );";
-        
-        client.query(query, (err, res) => { 
-            if (err) {
-                console.error(err); 
-                return; 
-            } 
-            console.log(`Table ${tablename} is successfully created`); 
-        }); 
-    },
-    endClient(client){
-        client.query(";", (err, res) => {  
-            if (err) {
-                console.error(err); 
-                return; 
-            } 
-            console.log(`End client`); 
-            client.end();
-        });
-    },
-    insertInto(client, tablename, elements){
-        let query = ` 
-        INSERT INTO ${tablename} ( 
-        `;
-        for(let i in elements){
-            query+=`${i}, `;
-        }
-        query=query.slice(0,-2);
-        query+=" ) VALUES (";
-        for(let i in elements){
-            if(typeof elements[i] =="string"){
-                query+=`'${elements[i]}', `;
-            }else{
-                query+=`${elements[i]}, `;
-            }
-        }
-        query=query.slice(0,-2);
-        query+=" );";
-        client.query(query, (err, res) => { 
-            if (err) { 
-                console.error(err); 
-                return; 
-            } 
-            console.log(`Elements in table ${tablename} is successfully insert`); 
-        }); 
-    },
-    deleteStrokes(client, tablename, where){
-
-        let query = ` 
-        DELETE FROM ${tablename} 
-        WHERE ${where} ;
-        `; 
-        if(where=="*"){
-            query=`DELETE FROM ${tablename};`
-        }
-
-        client.query(query, (err, res) => { 
-            if (err) { 
-                console.error(err); 
-                return; 
-            } 
-            console.log('Strokes delete successful'); 
-        });
+module.exports.getFrom = (client, columns, tablename) => {
+    let query = `SELECT `;
+    let answer;
+    for(let i in columns){
+        query+=`${columns[i]}, `;
     }
+    query=query.slice(0,-2);
+    query+=` FROM ${tablename};`;
+    //console.log(query);
+    client.query(query, (err, res,ans) => { 
+        if (err) {
+            console.error(err); 
+            return; 
+        } 
+        console.log(`Columns from ${tablename} is successfully got`);
+        answer=res;
+    }); 
+    return answer;
+};
+
+module.exports.createTable = (client, tablename, typesOfColumns) => {
+    let query = `CREATE TABLE ${tablename} ( `;
+    for(let i in typesOfColumns){
+        query+=`${i} ${typesOfColumns[i]}, `;
+    }
+    query=query.slice(0,-2);
+    query+=" );";
+    
+    client.query(query, (err, _) => { 
+        if (err) {
+            console.error(err); 
+            return; 
+        } 
+        console.log(`Table ${tablename} is successfully created`); 
+    }); 
+};
+
+module.exports.closeClient = (client) => {
+    client.query(";", (err, res) => {  
+        if (err) {
+            console.error(err); 
+            return; 
+        } 
+        console.log(`End client`); 
+        client.end();
+    });
+};
+
+module.exports.insertInto = (client, tablename, newObject) => {
+    let query = ` 
+    INSERT INTO ${tablename} ( 
+    `;
+    for(let i in newObject){
+        query+=`${i}, `;
+    }
+    query=query.slice(0,-2);
+    query+=" ) VALUES (";
+    for(let i in newObject){
+        if(typeof newObject[i] =="string"){
+            query+=`'${newObject[i]}', `;
+        }else{
+            query+=`${newObject[i]}, `;
+        }
+    }
+    query=query.slice(0,-2);
+    query+=" );";
+    client.query(query, (err,_) => { 
+        if (err) { 
+            console.error(err); 
+            return; 
+        } 
+        console.log(`Elements in table ${tablename} is successfully insert`); 
+    }); 
+};
+module.exports.deleteStrokes = (client, tablename, where) => {
+
+    let query = ` 
+    DELETE FROM ${tablename} 
+    WHERE ${where} ;
+    `; 
+    if(where=="*"){
+        query=`DELETE FROM ${tablename};`
+    }
+
+    client.query(query, (err, res) => { 
+        if (err) { 
+            console.error(err); 
+            return; 
+        } 
+        console.log('Strokes delete successful'); 
+    });
 };
 
 module.exports=exported;

8184921d0780d50201b5f3949230cc945dd471cc
Alexey <yankovskyalexey123@yandex.ru>
2023-07-20 16:29:06 +0300

    WS database control

--- src/app.js
+++ src/app.js
@@ -23,10 +23,10 @@ const client = psqlm.openClient(d8dconfig.dbuser,
 )
 
 wss.on("connection", ws => {
-    console.log("new ws client");
+    console.log(ws);
     ws.on("message", message => { 
-
-        console.log(message.toString());
+        message=JSON.parse(message.toString());
+        //psqlm.getObject(client,"testable",message.condition.key,message.condition.value,["*"]).then((r)=> {console.log(r.rows);ws.send(JSON.stringify(r.rows));});
     });
 });
 
@@ -35,13 +35,13 @@ app.use("/www", express.static(global.__basedirname + "/www"));
 app.use("/home", require("../controlers/home.js"));
 
 
-/*psqlm.createTable(client,"testable",{name:"varchar", id:"int", color:"varchar"});
-psqlm.addObject(client,"testable",{id:5,color:"blue"});
-psqlm.addObject(client,"testable",{id:6,color:"blue"});
-psqlm.getObject(client,"testable","","",["*"]).then((r)=>{console.log(r.rows)});
-psqlm.setObject(client,"testable","id",6,{color: "red", id: -10});
-psqlm.getObject(client,"testable","","",["*"]).then((r)=>{console.log(r.rows)});
-psqlm.deleteObject(client,"testable","*");*/
+//psqlm.createTable(client,"testable",{name:"varchar", id:"int", color:"varchar"});
+///psqlm.addObject(client,"testable",{id:5,color:"blue"});
+///psqlm.addObject(client,"testable",{id:6,color:"blue"});
+//psqlm.getObject(client,"testable","","",["*"]).then((r)=>{console.log(r.rows)});
+///psqlm.setObject(client,"testable","id",6,{color: "red", id: -10});
+//psqlm.getObject(client,"testable","","",["*"]).then((r)=>{console.log(r.rows)});
+//psqlm.deleteObject(client,"testable","*");
 psqlm.closeClient(client);
 
 server.listen(httpPort, host, ()=>{
